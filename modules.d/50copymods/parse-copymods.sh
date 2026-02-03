fce7161494a4294a6f1b5fa7409261ab6e689a72
Benjamin Drung <benjamin.drung@canonical.com>
2026-02-03 14:34:47 +0100

    copymods: simplify logic

--- modules.d/50copymods/parse-copymods.sh
+++ modules.d/50copymods/parse-copymods.sh
@@ -11,8 +11,4 @@ copymods=$(getarg copymods) || return 0
 
 # Make copymods= an alias for rd.driver.export=
 warn "Kernel command line option 'copymods' is deprecated, use 'rd.driver.export' instead."
-if [ -n "$copymods" ]; then
-    echo "rd.driver.export=$copymods" > /etc/cmdline.d/50-copymods.conf
-else
-    echo rd.driver.export > /etc/cmdline.d/50-copymods.conf
-fi
+echo "rd.driver.export${copymods:+=$copymods}" >> /etc/cmdline.d/50-copymods.conf
